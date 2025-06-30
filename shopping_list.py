e98e4fd65a1afad69172b43ebb86d6a22df9ec17
AnaJuliaFernandes <anajuliafcf@gmail.com>
2025-06-30 17:47:19 -0300

    add menu

---
+++ shopping_list.py
@@ -0,0 +1,53 @@
+#Objetivo: Desenvolver um aplicativo que gerencie uma lista de compras que permita adicionar,
+# remover ou listar os produtos adicionados nela.
+
+"""1-Menu de Opções: O sistema deve fornecer um menu de opções para o usuário interagir.
+As opções devem ser as seguintes:
+Adicionar produto
+Remover produto
+Pesquisar produtos
+Sair do programa"""
+
+"""2-Adicionar Produto: O usuário deve poder adicionar um novo produto à lista de compras.
+O sistema deve solicitar informações sobre o nome, unidade de medida, quantidade e descrição do produto.
+As opções de unidade devem ser:
+Quilograma
+Grama
+Litro
+Mililitro
+Unidade
+Metro
+Centímetro
+Essas opções devem aparecer quando o sistema perguntar a unidade de medida."""
+
+"""3-Controle de ID Automático: O sistema deve atribuir automaticamente um ID único para cada produto adicionado à 
+lista.
+4-Remover Produto: O usuário deve poder remover um produto da lista com base ID do produto. 
+O sistema deve solicitar o ID do produto que o usuário deseja remover.
+5-Pesquisar Produtos por Nome: O usuário deve poder pesquisar produtos na lista com base no nome ou parte do nome. 
+O sistema deve exibir os resultados correspondentes e fornecer a contagem total de produtos encontrados.
+6-Listar Todos os Produtos: O sistema deve ser capaz de exibir todos os produtos presentes na lista de compras, 
+se houver. Contudo, o menu não deve mostrar uma opção de “Listar produtos”. A exibição deverá ocorrer toda vez 
+que o menu principal for executado, acima dele.
+7-Cabeçalho do Aplicativo: Deve ser exibido um cabeçalho ao iniciar o aplicativo para fornecer uma saudação e 
+indicar que é uma Lista de Compras Simples.
+8-Feedback de Ação: Após a execução de uma ação (como adicionar ou remover um produto), o sistema deve fornecer 
+feedback indicando o resultado da ação.
+9-Tratamento de Entradas Inválidas: O sistema deve ser capaz de lidar com entradas inválidas do usuário e fornecer
+mensagens de erro apropriadas para orientar o usuário.
+10-Encerramento do Programa: O usuário deve poder encerrar o programa de forma adequada, escolhendo a opção de 
+saída no menu."""
+
+
+print("Faça sua lista de compras! Selecione a opção desejada.")
+print("1-Adicionar produto")
+print("2-Remover produto")
+print("3-Pesquisar produtos")
+print("4-Sair do programa")
+
+user_input = input("Digite a opção desejada: ")
+
+
+
+
+
